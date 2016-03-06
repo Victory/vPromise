7e2bc784645f127113f8c23fa8a8c1c9a27d6b36
Victory <git@dfhu.org>
2016-03-06 10:46:18 -0500

    some debugging

--- vPromise.js
+++ vPromise.js
@@ -1,7 +1,44 @@
 (function () {
   'use strict';
 
+  function debug() {
+    console.log(arguments);
+  }
+
+  debugger;
+
+  var states = {
+    UNFULFILLED: 0,
+    FULFILLED: 1,
+    REJECTED: 2,
+  };
+
   var vPromise = function (resolver) {
+    var state = states.UNFULFILLED;
+    var resultion;
+
+    var onFullfilled = function () {
+      debug('onFullfilled', arguments);
+    };
+
+    var onReject = function () {
+      debug('onReject', arguments);
+    };
+
+    if (typeof resolver === "function") {
+      resolver(onFullfilled, onReject);
+    }
+
+    var p = {};
+
+    p.then = function (fn) {
+      debug('then', fn);
+    };
+
+    p.catch = function (fn) {
+    };
+
+    return p;
   };
 
   if (typeof module != 'undefined') {
