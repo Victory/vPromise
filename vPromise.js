facade08fcbffedac22f8fc28656a3a0e6875947
Victory <git@dfhu.org>
2016-03-09 18:35:24 -0500

    debugger

--- vPromise.js
+++ vPromise.js
@@ -2,45 +2,85 @@
   'use strict';
 
   function debug() {
+    debugger;
     console.log(arguments);
   }
 
-  debugger;
+  function skip() {};
 
   var states = {
-    UNFULFILLED: 0,
+    PENDING: 0,
     FULFILLED: 1,
     REJECTED: 2,
   };
 
-  var vPromise = function (resolver) {
-    var state = states.UNFULFILLED;
-    var resultion;
+  var vPromise = function (fn) {
+    var state = states.PENDING;
+    var value;
+    var cbChains = [];
+    var that = this;
 
-    var onFullfilled = function () {
-      debug('onFullfilled', arguments);
-    };
-
-    var onReject = function () {
-      debug('onReject', arguments);
-    };
-
-    if (typeof resolver === "function") {
-      resolver(onFullfilled, onReject);
+    if (fn === skip) {
+      return;
     }
+
+    var onFullfilled = function (resolve) {
+      debug('onFullfilled');
+      if (state === states.FULFILLED || state == states.REJECTED) {
+        return;
+      }
+      state = state.FULFILLED;
+      resolve(); 
+    };
+
+    var onReject = function (reject, reason) {
+      debug('onReject');
+      if (state === states.FULFILLED || state == states.REJECTED) {
+        return;
+      }
+      state = states.REJECTED;
+      reject(); 
+    };
+
+    var handle = function (cur, resolve, reject, next) {
+      debug(cur);
+      if (cur.state !== states.pending) {
+        return;
+      }
+ 
+      try {
+        fn(onFullfilled(resolve), onReject(reject));
+      } catch (e) {
+        debug('catch', e);
+      } 
+    };
 
     var p = {};
 
-    p.then = function (fn) {
+    p.then = function (resolve, reject) {
       debug('then', fn);
-
-      return new VPromise(fn);
+      var next = new vPromise(skip);
+      //handle(this, resolve, reject, next);
+      return next;
     };
 
     p.catch = function (fn) {
     };
 
     return p;
+  };
+  vPromise._state = states.PENDING;
+
+  vPromise.resolve = function (value) {
+    var skipped = new vPromise(skip);
+    return doResolve(skipped, value);
+  };
+
+  function doResolve(skipped, value) {
+    console.log(skipped); 
+  }
+
+  vPromise.reject = function (value) {
   };
 
   if (typeof module != 'undefined') {
