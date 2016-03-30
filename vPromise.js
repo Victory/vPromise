4282fd69071a6309f27e3db22515572d5ff847c4
Victory <git@dfhu.org>
2016-03-30 18:58:37 -0400

    Start of rebuild

--- vPromise.js
+++ vPromise.js
@@ -20,123 +20,86 @@
   var vPromise = function (fn) {
     this._state = states.PENDING;
     this.value = null;
+    this.reason = null; // reasons can't change
     this._resolveChain = [];
-
+    this.fn = fn;
     var that = this;
 
-    var onFulfilled = function (resolve, val) {
-      if (typeof resolve !== 'function') { // 2.2.1
-        return;
-      }
+
+    var run = function (next) {
+    };
+
+    var startChain = function (chain, args) {
       var ii;
-      var chainLength = that._resolveChain.length;
+      var chainLength = chain.length;
       for (ii = 0; ii < chainLength; ii++) {
-        try {
-          that._resolveChain[ii](val);
-        } catch (e) {}
-      }
-      resolve(val);
-      if (that._state !== states.PENDING) {
-        that._state = states.FULFILLED;
+        run(chain[ii]);
       }
     };
 
-    var onReject = function (reject, reason) {
-      if (typeof reject !== 'function') { // 2.2.1
-        return;
-      }
-      if (that._state !== states.PENDING && that._state !== states.FULFILLED) {
-        that._state = states.REJECTED;
-        reject(reason);
-      }
+    var toArray = function (args) {
+      return Array.prototype.slice.call(args, 0);
     };
 
-    var handle = function (resolve, reject) {
+    this.resolve = function (value) {
       if (that._state === states.PENDING) {
-        return;
+        that._state = states.FULFILLED;
+        that.reason = toArray(arguments);
+        startChain(that._resolveChain, that.reason);
       }
 
-      var val = that.value;
-      asap(function () {
-          try {
-            fn(onFulfilled(resolve,val), onReject(reject, that.reason));
-          } catch (e) {
-            debug(e);
-          }
-      });
+      return this;
     };
 
-    if (fn !== skip) {
-      fn(function (value) {
-        that.value = value;
-        asap(function () {
-          if (that._state !== states.PENDING) {
-            return;
-          }
-          var ii;
-          var chainLength = that._resolveChain.length;
-          for (ii = 0; ii < chainLength; ii++) {
-            try {
-              that._resolveChain[ii](value);
-            } catch (e) {}
-          }
-          that._resolve(value);
-          that._state = states.FULFILLED;
-         });
-      },
-      function (reason) {
-        that.reason = reason;
-        asap(function () {
-          if (that._state !== states.PENDING) {
-            return;
-          }
-          that._reject(reason);
-          that._state = states.REJECTED;
-         });
-      });
-    }
+    this.reject = function (reason) {
+      if (that._state === states.REJECTED) {
+        that._state = states.REJECTED;
+      }
+      return this;
+    };
 
     this.then = function (resolve, reject) {
-      if (that._state === states.PENDING) {
-        if (typeof that._resolve == "function") {
-          that._resolveChain.push(that._resolve);
-        }
-        that._resolve = resolve;
-        that._reject = reject;
-        return;
+      var vP = new vPromise();
+      var onFulfilled;
+      var onRejected;
+      if (typeof resolve === "function") {
+        onFulfilled = function () {
+          vP.resolve.apply(that, arguments);
+        };
+      }
+      if (typeof reject === "function") {
+        onRejected = function () {
+          vP.reject.apply(that, arguments);
+        };
       }
 
-      var next = new vPromise(fn);
-      handle(resolve, reject);
-      next._state = -1; // placeholder
-      return next;
+      onFulfilled.next = onRejected.next = vP;
+
+      that._resolveChain.push({vp: vP, args: arguments});
+      this.resolve();
+      return vP;
     };
 
     return this;
   };
 
   vPromise.resolve = function (value) {
-    var skipped = new vPromise(skip);
-    return doResolve(skipped, value);
+    var resolve;
+    var vP = new vPromise(function (_resolve) {
+      resolve = _resolve;
+    });
+    vP.fn(function (value) {
+      return value;
+    });
+    resolve(value);
+    return vP;
   };
-
-  function doResolve(skipped, value) {
-    skipped._state = states.FULFILLED;
-    skipped.value = value;
-    return skipped;
-  }
 
   vPromise.reject = function (reason) {
-    var skipped = new vPromise(skip);
-    return doReject(skipped, reason);
+    var vP = new vPromise();
+    vP.reject(reason);
+    return vP;
   };
-
-  function doReject(skipped, reason) {
-    skipped._state = states.REJECTED;
-    skipped.reason = reason;
-    return skipped;
-  }
-
 
   if (typeof module != 'undefined') {
     module.exports = vPromise;
